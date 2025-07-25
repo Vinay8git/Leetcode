dae8746ac894d23f96997e4ffbca64158289bbf1
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-07-25 12:20:15 +0530

    Time: 7 ms (3.13%), Space: 42.7 MB (28.13%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution1 {
+
     public int maxSum(int[] nums) {
         // Map<Integer, Integer>hm=new HashMap<>();
         // int sum=0, max=Integer.MIN_VALUE;
@@ -15,17 +16,28 @@ class Solution {
         // }
         // return sum==0?max:sum;
 
-        Set<Integer>hm=new HashSet<>();
-        int sum=0, max=Integer.MIN_VALUE;
-        for(int n : nums)
-            hm.add(n);
-        for(int n : hm)
-        {
-            if(n>0)
-                sum+=n;
-            else
-                max=Math.max(max, n);
+        Set<Integer> hm = new HashSet<>();
+        int sum = 0, max = Integer.MIN_VALUE;
+        for (int n : nums) hm.add(n);
+        for (int n : hm) {
+            if (n > 0) sum += n; else max = Math.max(max, n); // If All Numbers are Negative : Select only Largest Negative Number.
         }
-        return sum==0?max:sum;
+        return sum == 0 ? max : sum;
+    }
+}
+
+class Solution {
+
+    public int maxSum(int[] nums) {
+        Set<Integer> positiveNumsSet = new HashSet<>();
+        for (int num : nums) {
+            if (num > 0) {
+                positiveNumsSet.add(num);
+            }
+        }
+        if (positiveNumsSet.isEmpty()) {
+            return Arrays.stream(nums).max().getAsInt();
+        }
+        return positiveNumsSet.stream().mapToInt(Integer::intValue).sum();
     }
 }
