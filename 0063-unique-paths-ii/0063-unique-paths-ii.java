04452991f95b470a2a165eb29c7692671e58cbe4
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-07-30 11:58:57 +0530

    Time: 0 ms (100%), Space: 41.6 MB (81.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -35,7 +35,7 @@ class Solution1 {
 }
 
 
-class Solution {
+class Solution2 {
     public int uniquePathsWithObstacles(int[][] og) 
     {
         int m = og.length;
@@ -63,5 +63,37 @@ class Solution {
             }
         }
         return dp[0][0];
+    }
+}
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] og) 
+    {
+        int m = og.length;
+        int n = og[0].length;
+
+        //Special Case
+        if(og[m-1][n-1] == 1) return 0;
+        if(og[0][0] == 1) return 0;
+
+        int[][] dp = new int[m+1][n+1];
+
+        //Base Case
+        dp[1][1] = 1;
+
+        for(int i=1; i<=m; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                if(i==1 && j==1)
+                    continue;
+
+                if(og[i-1][j-1]==1)
+                    dp[i][j]=0;
+
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
     }
 }
