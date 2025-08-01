5cc0319717be7298e07e9e547f0fa18371da9ccb
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-08-01 14:27:49 +0530

    Time: 135 ms (5.15%), Space: 44.6 MB (44.26%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -30,7 +30,47 @@ class Solution1 {
     }
 }
 
-class Solution {
+class Solution2 {
+    private void insertionSort(List<Integer> al)
+    {
+        for(int i=1;i<al.size();i++)
+        {
+            int key = al.get(i);
+            int j = i-1;
+            
+            while(j>=0 && al.get(j)>key)
+            {
+                al.set(j+1, al.get(j));
+                j-=1;
+            }
+            al.set(j+1, key);
+
+        }
+        // return al;
+    }
+    public ListNode insertionSortList(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(head != null)
+        {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        insertionSort(list);
+        ListNode dummy = new ListNode(-1);
+        temp = dummy;
+        for(int n : list)
+        {
+            dummy.next = new ListNode(n);
+            dummy = dummy.next;
+        }
+        return temp.next;
+    }
+}
+
+
+class Solution3 {
     private List<Integer> insertionSort(List<Integer> al)
     {
         for(int i=1;i<al.size();i++)
@@ -58,6 +98,45 @@ class Solution {
         }
 
         list = insertionSort(list);
+        ListNode dummy = new ListNode(-1);
+        temp = dummy;
+        for(int n : list)
+        {
+            dummy.next = new ListNode(n);
+            dummy = dummy.next;
+        }
+        return temp.next;
+    }
+}
+
+class Solution {
+    private void insertionSort(List<Integer> al, int n)
+    {
+        if(n==0)
+            return;
+
+        insertionSort(al, n-1);
+
+        int j = n-1;
+        int key = al.get(n);
+        while(j>=0 && al.get(j) > key)
+        {
+            al.set(j+1, al.get(j));
+            j-=1;
+        }
+        al.set(j+1, key);
+        return;
+    }
+    public ListNode insertionSortList(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(head != null)
+        {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        insertionSort(list, list.size()-1);
         ListNode dummy = new ListNode(-1);
         temp = dummy;
         for(int n : list)
