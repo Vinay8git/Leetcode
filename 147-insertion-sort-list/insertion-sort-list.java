0261dc50a0e8cb6b82e1331018044ff1eb20531e
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-08-01 14:38:30 +0530

    Time: 126 ms (5.15%) | Memory: 44.6 MB (56.99%) - LeetSync

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -109,7 +109,7 @@ class Solution3 {
     }
 }
 
-class Solution {
+class Solution4 {
     private void insertionSort(List<Integer> al, int n)
     {
         if(n==0)
@@ -137,6 +137,47 @@ class Solution {
         }
 
         insertionSort(list, list.size()-1);
+        ListNode dummy = new ListNode(-1);
+        temp = dummy;
+        for(int n : list)
+        {
+            dummy.next = new ListNode(n);
+            dummy = dummy.next;
+        }
+        return temp.next;
+    }
+}
+
+class Solution {
+    private List<Integer> insertionSort(List<Integer> al, int i, int n)
+    {
+        if(i>=n)
+            return al;
+
+        // insertionSort(al, n-1);
+
+        int j = i-1;
+        int key = al.get(i);
+        while(j>=0 && al.get(j) > key)
+        {
+            al.set(j+1, al.get(j));
+            j-=1;
+        }
+        al.set(j+1, key);
+
+        return insertionSort(al, i+1, n);
+
+    }
+    public ListNode insertionSortList(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(head != null)
+        {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        insertionSort(list, 1, list.size());
         ListNode dummy = new ListNode(-1);
         temp = dummy;
         for(int n : list)
