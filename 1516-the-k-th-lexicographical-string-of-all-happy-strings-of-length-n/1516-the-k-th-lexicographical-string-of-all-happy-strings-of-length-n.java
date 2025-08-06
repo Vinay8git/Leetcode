3cc47d9a741e37ada58802e61b65ff330354135d
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-08-06 12:33:03 +0530

    Time: 24 ms (19.72%), Space: 45.3 MB (29.18%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,4 +1,42 @@
-class Solution 
+class Solution1 {
+    int count = 0;
+    String res;
+    private void compose(String ans, int n, int k)
+    {
+        if(n==0)
+        {
+            count++;
+            if(count==k)
+                res=ans;
+
+            return;
+        }
+        if(count>=k) return;
+
+        if(ans.isEmpty() || ans.charAt(ans.length()-1) != 'a')
+            compose(ans+"a", n-1, k);  
+
+        if(ans.isEmpty() || ans.charAt(ans.length()-1) != 'b')
+            compose(ans+"b", n-1, k);
+
+        if(ans.isEmpty() || ans.charAt(ans.length()-1) != 'c')
+            compose(ans+"c", n-1, k);
+
+        return;
+
+
+    }
+    public String getHappyString(int n, int k) 
+    {
+
+        compose("", n, k);
+        return res==null? "" : res;
+    }
+}
+
+
+
+class Solution2 
 {
     int count = 0;
     String res;
@@ -39,18 +77,88 @@ class Solution
         else if(k>(ser/3) && k <= 2*(ser/3))
         {
             k = k - (ser/3);
-
             compose("b", n-1, k);
         }
-
         else
         {
             k = k - 2*(ser/3);
-
             compose("c", n-1, k);
         }
 
         
-        return res==null? "" : res;
+        return res == null ? "" : res;
+    }
+}
+
+
+
+class Solution 
+{
+    // int count = 0;
+    // String res;
+    // private void compose(String ans, int n, int k)
+    // {
+    //     if(n==0)
+    //     {
+    //         count++;
+    //         if(count==k)
+    //             res=ans;
+
+    //         return;
+    //     }
+    //     if(count>=k) return;
+
+    //     if(ans.charAt(ans.length()-1) != 'a')
+    //         compose(ans+"a", n-1, k);  
+
+    //     if(ans.charAt(ans.length()-1) != 'b')
+    //         compose(ans+"b", n-1, k);
+
+    //     if(ans.charAt(ans.length()-1) != 'c')
+    //         compose(ans+"c", n-1, k);
+
+    //     return;
+
+
+    // }
+    public String getHappyString(int n, int k) 
+    {
+        int level = 3*(int)Math.pow(2, n-1);
+        if(k>level) return "";
+
+        Queue<String> q = new LinkedList<>();
+
+        if(n==1)
+        {
+            if(k==1) return "a";
+            if(k==2) return "b";
+            if(k==3) return "c";
+            // if(k>3) return "";
+        }
+        q.offer("a");
+        q.offer("b");
+        q.offer("c");
+        // n-=1;
+
+
+        while(!q.isEmpty())
+        {
+            String parent = q.poll();
+            for(char c='a'; c<='c'; c++)
+            {
+                if(parent.charAt(parent.length()-1) == c)
+                    continue;
+
+                if(parent.length()+1 == n)
+                    k--;
+
+                String res = parent+c;
+                if(k==0) return res;
+
+                q.offer(res);
+            }
+        }
+        return "";
+
     }
 }
