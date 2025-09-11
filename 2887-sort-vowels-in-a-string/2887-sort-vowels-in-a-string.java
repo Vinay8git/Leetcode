5996a5b51f8bf0d8a62ecb9e032043dfbc3019d9
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-09-11 14:02:19 +0530

    Time: 20 ms (87.89%), Space: 45.5 MB (96.78%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public String sortVowels(String s) {
         List<Character> vw = new ArrayList<>();
         for(char c : s.toCharArray())
@@ -23,3 +23,84 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+
+//In-Place Sorting Without Using Extra Space.
+class Solution2 {
+    private boolean isVowel(char c)
+    {
+        if(c=='a' || c=='A' || c=='e' || c=='E' || c=='i' || c=='I' || c=='o' || c=='O' ||     c=='u' || c=='U')
+            return true;
+        return false;
+    }
+    private String swap(String s, int i, int j)
+    {
+        String pre = s.substring(0,i);
+        pre=pre+s.charAt(j);
+        pre=pre+s.substring(i+1,j);
+        pre=pre+s.charAt(i);
+        pre=pre+s.substring(j+1);
+        return pre;
+    }
+    public String sortVowels(String s) {
+        int len = s.length();
+        for(int i=0;i<len-1;i++)
+        {
+            if(isVowel(s.charAt(i)))
+            {
+                for(int j=i+1;j<len;j++)
+                {
+                    if(isVowel(s.charAt(j)))
+                    {
+                        if(s.charAt(i)>s.charAt(j))
+                        {
+                            s=swap(s, i, j);
+                        }
+                    }
+                }
+            }
+        }
+        return s;
+    }
+}
+
+
+class Solution {
+    // Returns true if the character is a vowel.
+    boolean isVowel(Character c) {
+        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i'
+                || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
+    }
+
+    public String sortVowels(String s) {
+        int[] count = new int[1000];
+
+        // Store the frequencies for each character.
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                count[c - 'A']++;
+            }
+        }
+
+        // Sorted string having all the vowels.
+        String sortedVowel = "AEIOUaeiou";
+        StringBuilder ans = new StringBuilder();
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!isVowel(s.charAt(i))) {
+                ans.append(s.charAt(i));
+            } else {
+                // Skip to the character which is having remaining count.
+                while (count[sortedVowel.charAt(j) - 'A'] == 0) {
+                    j++;
+                }
+
+                ans.append(sortedVowel.charAt(j));
+                count[sortedVowel.charAt(j) - 'A']--;
+            }
+        }
+        return ans.toString();
+    }
+};
