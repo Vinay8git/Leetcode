3b30eaecf00109d737996e5399af40a7efd287df
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-10-29 11:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (58.12%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,18 +1,33 @@
+// class Solution {
+//     private boolean checkBits(int n)
+//     {
+//         while(n>0)
+//         {
+//             if((n&1)==0) return false;
+//             n>>=1;
+//         }
+//         return true;
+//     }
+//     public int smallestNumber(int n) {
+//         for(int i=n;;i++)
+//         {
+//             if(checkBits(i))
+//                 return i;
+//         }
+//     }
+// }
+
+
+
 class Solution {
-    private boolean checkBits(int n)
-    {
-        while(n>0)
-        {
-            if((n&1)==0) return false;
-            n>>=1;
-        }
-        return true;
-    }
     public int smallestNumber(int n) {
-        for(int i=n;;i++)
-        {
-            if(checkBits(i))
-                return i;
-        }
+        if (n <= 1) return 1;
+        // n--;                     // handle exact powers
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        return n;
     }
 }
