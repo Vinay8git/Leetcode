3b08a355f76c2fd6a08c03e1357486dc87839d49
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-11-05 21:46:18 +0530

    Time: 41 ms (63.77%) | Memory: 47 MB (9.06%) - LeetSync

--- 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.java
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public String largestMerge(String word1, String word2) {
 
         String s="";
@@ -43,5 +43,32 @@ class Solution {
         }
 
         return s;
+    }
+}
+
+class Solution {
+    public String largestMerge(String word1, String word2) {
+
+        int i = 0;
+        int j = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (i < word1.length() && j < word2.length()) {
+
+            // Compare remaining suffixes
+            if (word1.substring(i).compareTo(word2.substring(j)) > 0) {
+                sb.append(word1.charAt(i));
+                i++;
+            } else {
+                sb.append(word2.charAt(j));
+                j++;
+            }
+        }
+
+        // Append remaining tail
+        if (i < word1.length()) sb.append(word1.substring(i));
+        if (j < word2.length()) sb.append(word2.substring(j));
+
+        return sb.toString();
     }
 }
