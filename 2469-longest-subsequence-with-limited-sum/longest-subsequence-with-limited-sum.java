c02ed5df16fcf9c02760065f48d76e85752038bd
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-11-07 12:50:18 +0530

    Time: 8 ms (19.17%) | Memory: 47 MB (5.50%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public int[] answerQueries(int[] nums, int[] queries) {
         int[] res = new int[queries.length];
         int count=0;
@@ -16,6 +16,41 @@ class Solution {
                 else break;
             }
             res[k]=count;
+        }
+        return res;
+    }
+}
+
+
+
+
+class Solution {
+    private int findIdx(int[] nums, int target)
+    {
+        int low=0, high=nums.length-1, ans=-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(nums[mid]<=target)
+            {
+                low=mid+1;
+                ans=mid;
+            }
+            else
+                high=mid-1;
+        }
+        return ans==-1?0:ans+1;
+    }
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int[] res = new int[queries.length];
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+            nums[i]=nums[i]+nums[i-1];
+        
+        for(int k=0;k<queries.length;k++)
+        {
+            res[k]=findIdx(nums, queries[k]);
         }
         return res;
     }
