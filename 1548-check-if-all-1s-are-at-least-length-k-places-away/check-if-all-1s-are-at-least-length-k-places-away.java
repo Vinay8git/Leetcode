8bfe8d8f7ec8874854943bc5b0f33897821cb828
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-11-17 11:56:05 +0530

    Time: 4 ms (13.43%) | Memory: 65.6 MB (9.43%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -81,7 +81,7 @@ class Solution4 {
 
 
 
-class Solution {
+class Solution5 {
     public boolean kLengthApart(int[] nums, int k) {
         int n = nums.length;
         int s=0, e=0;
@@ -101,6 +101,32 @@ class Solution {
             }
             e++;
         }
+        return true;
+    }
+}
+
+
+
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int n = nums.length;
+        // int key = 1;
+        for(int i=0;i<n;i++)
+        {
+            
+            if(nums[i] == 1)
+            {
+                if(hm.containsKey(nums[i]))
+                {
+                    if(i-hm.get(nums[i])-1 < k) return false;
+                    hm.put(nums[i], i);
+                }
+                else hm.put(nums[i], i);
+            }
+        }
+
         return true;
     }
 }
