3c136d861e20331fc139ef069d5c483b47202749
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-11-17 12:07:38 +0530

    Time: 1 ms (99.71%), Space: 65.4 MB (15.14%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -81,7 +81,7 @@ class Solution4 {
 
 
 
-class Solution {
+class Solution5 {
     public boolean kLengthApart(int[] nums, int k) {
         int n = nums.length;
         int s=0, e=0;
@@ -101,6 +101,58 @@ class Solution {
             }
             e++;
         }
+        return true;
+    }
+}
+
+
+
+
+class Solution6 {
+    public boolean kLengthApart(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int n = nums.length;
+        // int key = 1;
+        for(int i=0;i<n;i++)
+        {
+            
+            if(nums[i] == 1)
+            {
+                if(hm.containsKey(nums[i]))
+                {
+                    if(i-hm.get(nums[i])-1 < k) return false;
+                    hm.put(nums[i], i);
+                }
+                else hm.put(nums[i], i);
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) 
+    { 
+        int n = nums.length;
+        int seen = -1;
+        for(int i=0;i<n;i++)
+        {
+            
+            if(nums[i] == 1)
+            {
+                if(seen != -1)
+                {
+                    if(i-seen-1 < k) return false;
+                    seen = i;
+                }
+                else seen = i;
+            }
+        }
+
         return true;
     }
 }
