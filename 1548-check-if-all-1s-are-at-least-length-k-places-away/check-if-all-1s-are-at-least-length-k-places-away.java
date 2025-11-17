35b9facc89323650a4d112d08af85c49b013c52a
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-11-17 09:14:35 +0530

    Time: 1 ms (99.71%) | Memory: 65.8 MB (5.71%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -0,0 +1,94 @@
+class Solution1 {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i] == 1 && nums[j] == 1)
+                    if((j-i-1) < k) return false;
+            
+            }
+        }
+        return true;
+    }
+}
+
+
+
+class Solution2 {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i] == 0) continue;
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[j] == 1)
+                    if((j-i-1) < k) return false;
+            
+            }
+        }
+        return true;
+    }
+}
+
+
+
+class Solution3 {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i] == 1)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    if(nums[j] == 1 && (j-i-1) < k) return false;
+                        
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i] == 1)
+            {
+                for(int j=i+1;j<n;j++)
+                {
+                    if(nums[j] == 1)
+                    {
+                        if((j-i-1) < k) return false;
+                        else break;
+                    }
+                        
+                }
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+class Solution5 {
+    public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        int s=0, e=0;
+        while(s<n)
+        {
+
+        }
+        return true;
+    }
+}
